6f41e919bf7bd569b27812d9bf37a28476592add
m <mahhov1@gmail.com>
2017-06-09 08:40:08 -0700

    base working + tabs added

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        run: {
+            localServer: {
+                cmd: 'watch-http-server',
+                args: [
+                    '-o',
+                    '-a',
+                    'localhost'
+                ]
+            },
+            sassWatch: {
+                cmd: 'sass',
+                args: [
+                    '--watch',
+                    'public/:public/'
+                ],
+                options: {
+                    wait: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-run');
+
+    grunt.registerTask('dev', ['run:sassWatch', 'run:localServer']);
+};
