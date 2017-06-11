6922e704bfe96488bd5e5b879feb4428834bed78
manukfarmers <mahhov@yahoo.com>
2017-06-11 16:34:59 -0700

    server.js from heroku

--- server.js
+++ server.js
@@ -1,31 +1,8 @@
-// server.js
-
-// set up ========================
-var express = require('express');
-var app = express();                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-
-// configuration =================
-
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride());
-
-// listen (start app with node server.js) ======================================
-var port = process.env.PORT || 8085;
-app.listen(port);
-console.log("App listening on port " + port);
-
-// routes ======================================================================
-
-app.get('*', function (req, res) {
-    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
-
-var launcher = require('simple-autoreload-server');
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/public'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8085);
